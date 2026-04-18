a2db65ad3ddc973fad78c3bce287a10a41dae177
Jack Flori <24jflori@gmail.com>
2026-04-18 02:47:05 -0400

    remove extraneous comments

--- finalProject.py
+++ finalProject.py
@@ -1,26 +1,6 @@
 from cmu_graphics import *
 import copy
 
-'''
-Feature List:
-- Main Menu, Win/Lose Screen
-- Basic Sokoban Controls/Rules
-- 3 Levels of Increasing Difficulty
-- Reset Level with Enter
-- Toggle Hard/Timed Mode
-- Move Counter Displayed After Win
-- Looped Background Music with Play/Pause Button
-- Level Completion Sound Effect
- 
-Grading Shortcuts:
-0: Menu Screen
-1: Level 1
-2: Level 2
-3: Level 3
-4: Win Screen
-5: Lose Screen (hard mode)
-'''
-
 class Level:
     def __init__(self, board, balls, player):
         self.board = copy.deepcopy(board)
